7bc2af2bef2abde12e38cd8b08a5038bec0fdf36
fatihcelebi <fatihcelebi13@gmail.com>
2021-07-01 13:06:48 +0300

    all files added

---
+++ Birlestirme_Uygulamaları/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Birlestirme_Uygulamaları
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection baglanti = new SqlConnection(@"Data Source=DESKTOP-2HOS6DT\SQLEXPRESS;Initial Catalog=Test;Integrated Security=True");
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter("select HAREKETID, URUNAD, (AD+ ' ' +SOYAD) as MUSTERI, ADSOYAD as PERSONEL, TBLHAREKETLER.FIYAT from TBLHAREKETLER inner join TBLURUNLER on TBLHAREKETLER.URUN=TBLURUNLER.URUNID inner join TBLMUSTERI on TBLHAREKETLER.MUSTERI = TBLMUSTERI.ID inner join TBLPERSONEL on TBLHAREKETLER.PESONEL = TBLPERSONEL.ID", baglanti);
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+    }
+}
